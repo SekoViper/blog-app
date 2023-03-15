fd039e24f636958d33a57d0c5999320ac874d678
SekoViper <damehlivingstone1@gmail.com>
2023-03-15 07:45:21 +0000

    Modify code to access post instances.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
   def index
-    puts Post.where(author_id: params[:user_id])
+    @posts = User.find_by(id: params[:user_id])&.posts || []
   end
 
   def show
     puts Post.where(author_id: params[:user_id], id: params[:id])
+    @post = Post.find_by(author_id: params[:user_id], id: params[:id]) || 'There are no posts'
   end
 end
