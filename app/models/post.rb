d772ff3c82491bfc9c08ed502221c40df92cead8
SekoViper <damehlivingstone1@gmail.com>
2023-03-15 15:36:55 +0000

    Add pagination to the Post model and set the default number of posts per page to 3.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  paginates_per 3
+
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, :likes_counter, comparison: { greater_than_or_equal_to: 0 },
                                                numericality: { only_integer: true }
