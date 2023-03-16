9113b233d03658afb2f1c9420b66db4c7ba264ea
SekoViper <damehlivingstone1@gmail.com>
2023-03-16 17:24:09 +0000

    Render comment partial with local variable 'comment'.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,16 @@
 class PostsController < ApplicationController
   def index
     @user = User.find_by(id: params[:user_id])
-    if @user.present?
-      @posts = @user.posts.page params[:page]
-    else
-      @posts = []
-    end
-
+    @posts = if @user.present?
+               @user.posts.page params[:page]
+             else
+               []
+             end
   end
 
   def show
-    @post = Post.find_by(author_id: params[:user_id], id: params[:id]) || 'There are no posts'
+    @post = Post.includes(:comments).find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    @post = 'There are no posts'
   end
 end
