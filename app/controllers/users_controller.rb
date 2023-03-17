2e486f7e3e6162bc42388b92d9419de6112dcee1
SekoViper <damehlivingstone1@gmail.com>
2023-03-17 12:08:37 +0000

    Added create action to UsersController with user_params method to permit the name, bio, and photo attributes of the user parameter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,20 @@ class UsersController < ApplicationController
   def show
     @user = User.includes(:posts).find_by(id: params[:id]) || 'There is no user'
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'User saved successfully'
+      redirect_to users_path
+    else
+      flash.now[:error] = "Error: couldn't save user"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :bio, :photo)
+  end
 end
