f8a272e079c4bee08f21a2f272634ed12014e5a2
SekoViper <damehlivingstone1@gmail.com>
2023-03-17 12:05:09 +0000

    Added new and create actions to CommentsController.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,25 @@ class PostsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     @post = 'There are no posts'
   end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = 'Post saved successfully'
+      redirect_to users_path
+    else
+      render :new, alert: 'Error occured!'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
