6365264b44f4b978b4d2364f48907fd82690de5e
SekoViper <damehlivingstone1@gmail.com>
2023-03-22 13:43:41 +0000

    Fix linter errors.

--- spec/feature/user_system_spec.rb
+++ spec/feature/user_system_spec.rb
@@ -1,63 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe '', type: :system do
-    it 'displays all users' do
-      # display a page
-      visit users_path
+  it 'displays all users' do
+    # display a page
+    visit users_path
 
-      # assert the displayed page
-      assert_text 'Seko'
+    # assert the displayed page
+    assert_text 'Seko'
 
-      # navigate to the show page
-      sleep(1)
-      click_link('Seko')
+    # navigate to the show page
+    sleep(1)
+    click_link('Seko')
 
-      # assert the displayed page
-      sleep(1)
-      assert_text 'Bio'
-    end
+    # assert the displayed page
+    sleep(1)
+    assert_text 'Bio'
+  end
 
-    it 'displays the user with their posts' do
-      # display a all users
-      visit users_path
+  it 'displays the user with their posts' do
+    # display a all users
+    visit users_path
 
-      # assert the displayed page
-      assert_text 'Seko'
+    # assert the displayed page
+    assert_text 'Seko'
 
-      # click on a user from the displayed page
-      sleep(1)
-      click_link('Seko')
+    # click on a user from the displayed page
+    sleep(1)
+    click_link('Seko')
 
-      # assert the displayed page has a post with text 'second' and 'Show'
-      assert_text 'super'
-      assert_text 'second'
-      assert_text 'Show all posts'
+    # assert the displayed page has a post with text 'second' and 'Show'
+    assert_text 'super'
+    assert_text 'second'
+    assert_text 'Show all posts'
 
-      # click on a post from the list of posts
-      sleep(1)
-      page.click_on 'first'
+    # click on a post from the list of posts
+    sleep(1)
+    page.click_on 'first'
 
-      # assert the displayed page
-      sleep(1)
-      assert_text 'Seko'
-    end
+    # assert the displayed page
+    sleep(1)
+    assert_text 'Seko'
+  end
 
-    it 'displays the index page of the post' do
-      # display a all users
-      visit users_path
+  it 'displays the index page of the post' do
+    # display a all users
+    visit users_path
 
-      # assert the displayed page
-      assert_text 'Seko'
+    # assert the displayed page
+    assert_text 'Seko'
 
-      # click on a user from the displayed page
-      sleep(1)
-      click_on 'Seko'
+    # click on a user from the displayed page
+    sleep(1)
+    click_on 'Seko'
 
-      # navigate to posts path
-      sleep(1)
-      click_button 'Show all posts'
+    # navigate to posts path
+    sleep(1)
+    click_button 'Show all posts'
 
-      # assert the displayed page
-      sleep(1)
-    end
+    # assert the displayed page
+    sleep(1)
+  end
 end
