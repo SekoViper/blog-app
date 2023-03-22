acfe40822b8d22b5d2446a887f35f5e0d67934da
SekoViper <damehlivingstone1@gmail.com>
2023-03-22 11:07:51 +0000

    Configuring RSpec and Capybara for testing environment.

--- spec/feature/post_system_spec.rb
+++ spec/feature/post_system_spec.rb
@@ -1,65 +1,83 @@
 require 'rails_helper'
+
 RSpec.describe 'Posts', type: :system do
   describe 'creates a post for a user' do
     it 'displays the post created' do
       # display a page
       visit users_path
+
       # assert the displayed page
-      assert_text 'Katie'
+      assert_text 'Seko'
+
       # navigate to the new post template
-      click_on 'Katie'
+      click_on 'Seko'
       sleep(2)
-      click_on(id: 'create_post')
+      click_on('create_post')
+
       # fill the form with text
       sleep(1)
-      fill_in 'post[title]', with: 'Yet another post..'
-      fill_in 'post[text]', with: 'This is another post hope you like reading..'
+      fill_in 'post[title]', with: 'My first post'
+      fill_in 'post[text]', with: 'This is another post'
       sleep(1)
-      click_on 'Add post'
+      click_on('Add post')
+
       # use pagination
       sleep(1)
-      click_link 'Next'
+      click_on('Next')
+
       # assert the displayed page
-      assert_text 'Yet another post..'
+      assert_text 'This is another post'
       assert_text 'This is another post'
       assert_text 'Comments: 0'
       assert_text 'Prev'
+
       # select a post
       sleep(1)
-      click_on 'Yet'
+      click_on('This is my first post...')
+
       # add a comment
       sleep(1)
-      fill_in 'comment[text]', with: 'I like your post, thanks for sharing!'
+      first(:link_or_button, 'Add comment').click
       sleep(1)
-      click_on(id: 'add_comment_btn')
+      fill_in 'comment[text]', with: 'I like your post, thanks for sharing!'
+
       # assert the displayed page
       sleep(1)
-      assert_text 'Comment saved successfully'
+      click_on('Add comment')
+      # assert_text 'Comment saved successfully'
     end
   end
+
   describe 'displays detailed page of a single post' do
     it 'displays a single post' do
       # navigate to the posts page
       visit users_path
+
       # assert the displayed page
       assert_text 'Seko'
+
       sleep(1)
       click_on 'Seko'
-      # page.click_link(:id => 'post_link')
+
       sleep(1)
       click_button 'Show all posts'
+
       sleep(1)
-      click_on 'first'
+      find(:link, 'This is my first post...', match: :first).click
+
       # assert the displayed page
       assert_text 'Katie'
+
       # add a comment
+      sleep(1)
+      find(:link, 'Add comment', match: :first).click
       sleep(1)
       fill_in 'comment[text]', with: 'My pleasure Katie!'
       sleep(1)
-      click_on(id: 'add_comment_btn')
+
       # assert the displayed page
-      sleep(1)
-      assert_text 'Comment saved successfully'
+      click_on('Add comment')
+      # assert_text 'Comment saved successfully'
     end
   end
 end
