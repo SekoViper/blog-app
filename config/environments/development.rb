63bcbfe500e55257505d865a6a2993d7564398a7
SekoViper <damehlivingstone1@gmail.com>
2023-03-25 08:53:54 +0000

    Remove unwanted code frmo code base.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,18 +45,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
 
-  # config.action_mailer.delivery_method = :smtp
-
-  # config.action_mailer.smtp_settings = {
-  #   user_name:     'damehlivingstone1@gmail.com',
-  #   password:      'bwbrikyidfxwctbc',
-  #   domain:        'localhost:3000',
-  #   address:       'smtp.gmail.com',
-  #   port:          '587',
-  #   authentication: :plain,
-  #   enable_starttls_auto: true,
-  # }
-
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
